bf7429619137fffdcd452d7f445bd8898f3c9565
risaltte <risaltte@gmail.com>
2022-10-24 13:40:12 -0400

    Custom Select

--- js/home.js
+++ js/home.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const buttonContainers = document.querySelectorAll(".button-container");
     const buttonAdd = document.querySelector("#buttom-add");
 
+   
+
     buttonAdd.addEventListener('click', () => {
         // overlay.toggleAttribute('hidden');
         overlay.classList.toggle("add-appointment");
@@ -11,4 +13,26 @@ document.addEventListener("DOMContentLoaded", () => {
             buttonContainer.classList.toggle("add-appointment");
         });
     });
+
+
+    // Custom select
+    const appointmentTypeInput = document.querySelector("#appointment-type");
+    const selectOptionsList = document.querySelector(".select-DropDown");
+    const selectOptions = document.querySelectorAll(".select-option");
+
+    appointmentTypeInput.addEventListener('focus', () => {
+        selectOptionsList.style.display = "block";
+    });
+
+    appointmentTypeInput.addEventListener('blur', () => {
+        selectOptionsList.style.display = "none";
+    });
+
+    selectOptions.forEach((selectOption) => {
+        selectOption.addEventListener('mousedown', (event) => {
+            const appointmentTypeSelected = event.target.innerHTML;
+            appointmentTypeInput.value = appointmentTypeSelected;
+        });
+    });
+
 }) 
